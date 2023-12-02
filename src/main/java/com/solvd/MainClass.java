e2f03c795b1b3debc8fb2c113bf786ef67dab73a
PiotrPor <piotr.porembinski@outlook.com>
2023-12-02 22:37:29 +0100

    Code reduction (part 2)
    All operations in main() are done with fewer lines of code.
    HashMap is no longer used.
    A few variables are no longer used.

--- src/main/java/com/solvd/MainClass.java
+++ src/main/java/com/solvd/MainClass.java
@@ -6,59 +6,37 @@ import org.apache.commons.io.FileUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.ArrayList; //?
+import java.util.ArrayList;
 import java.io.File;
-import java.util.Map;
-import java.util.HashMap;
 
 public class MainClass {
-    public static void main(String[] args) {
-        List<String> linesFromFile;
-        List<String> separateWords = new ArrayList<>();
-        Map<String,Integer> wordQuantities = new HashMap<>();
-
+    public static void main(String[] args) throws IOException {
         String titleOfTextFile = "about_Titanium.txt"; //will be needed later
-        try {
-            String filepath = "src/main/resources/" + titleOfTextFile;
-            linesFromFile = FileUtils.readLines(FileUtils.getFile(filepath), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        String filepath = "src/main/resources/" + titleOfTextFile;
+        String fileAsString = FileUtils.readFileToString(FileUtils.getFile(filepath), StandardCharsets.UTF_8);
+
+        fileAsString = StringUtils.lowerCase(fileAsString);
+
+        for(int i=0; i<fileAsString.length(); i++) {
+            char cc = fileAsString.charAt(i);
+            if(!(Character.isLetter(cc) || cc == ' ')) {
+                fileAsString = fileAsString.replace(cc,' ');
+            }
         }
 
-        String[] tempArray;
-        for (String s1 : linesFromFile) {
-            s1 = StringUtils.lowerCase(s1);
-            tempArray = StringUtils.split(s1," ");
-            for(String s2 : tempArray) {
+        List<String> separateWords = new ArrayList<>();
+
+        for(String s2 : StringUtils.split(fileAsString," ")) {
+            s2 = s2.trim(); //remove whitespaces from beginning or end
+            if(!separateWords.contains(s2)) {
                 separateWords.add(s2);
             }
         }
 
-        for(String word : separateWords) {
-            if(wordQuantities.containsKey(word)) {
-                wordQuantities.put(word, wordQuantities.get(word)+1);
-            } else {
-                wordQuantities.put(word, 1);
-            }
-        }
-
-        int amountOfUnique = 0;
-        for(Map.Entry<String,Integer> aboutWord : wordQuantities.entrySet()) {
-            if(aboutWord.getValue() == 1) {
-                amountOfUnique += 1;
-            }
-        }
-
         String descriptionOfResult = "In \""+ titleOfTextFile +"\" there were ";
-        descriptionOfResult += amountOfUnique +" unique words.\n";
+        descriptionOfResult += separateWords.size() +" unique words.";
         File fileWithResult = new File("results.txt");
-        try {
-            //FileUtils.touch(fileWithResult);
-            //before writing to file, creates a file if it doesn't exist
-            FileUtils.writeStringToFile(fileWithResult, descriptionOfResult, StandardCharsets.UTF_8, true);
-            //FileUtils.write(fileWithResult, descriptionOfResult, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException("IO exception: "+ e.getMessage());
-        }
+        // write/overwrite to file, create file if it doesn't exist
+        FileUtils.writeStringToFile(fileWithResult, descriptionOfResult, StandardCharsets.UTF_8, false);
     }
 }
